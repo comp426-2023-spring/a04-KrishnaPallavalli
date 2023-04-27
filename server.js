0e716e85fc9f1aff1b56b0d4fcde026b96febda0
KrishnaPallavalli <32885657+KrishnaPallavalli@users.noreply.github.com>
2023-04-27 14:24:38 -0400

    Tried a solution to fix server.js problem.

--- server.js
+++ server.js
@@ -5,36 +5,31 @@ import { rps, rpsls } from './lib/rpsls.js'
 import express from 'express'
 var app = express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/app', (req, res, next) => {
     res.status(200).send("200 OK")
 })
-
 app.get('/app/rps', (req, res, next) => {
     res.status(200).send(rps())
 })
-
 app.get('/app/rpsls', (req, res, next) => {
     res.status(200).send(rpsls())
 })
-
 app.post('/app/rps/play', (req, res, next) => {
     res.status(200).send(rps(req.body.shot()))
 })
-
 app.get('/app/rps/play/:shot', (req, res, next) => {
     res.status(200).send(rps(req.params.shot))
 })
-
 app.post('/app/rpsls/play', (req, res, next) => {
     res.status(200).send(rpsls(req.body.shot))
 })
-
 app.get('/app/rpsls/play/:shot', (req, res, next) => {
     res.status(200).send(rps(req.params.shot))
 })
-
 app.get('*', (req, res, next) => {
     res.status(404).send('404 NOT FOUND')
 })
-
 app.listen(port)
