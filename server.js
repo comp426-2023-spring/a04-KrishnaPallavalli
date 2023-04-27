6f85cb52b95457b7f265f37185ff13cc4cb4e652
KrishnaPallavalli <32885657+KrishnaPallavalli@users.noreply.github.com>
2023-04-27 14:30:41 -0400

    finished

--- server.js
+++ server.js
@@ -4,37 +4,45 @@ const port = args.port || 5000
 import { rps, rpsls } from './lib/rpsls.js'
 import express from 'express'
 var app = express()
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/app', (req, res, next) => {
+app.get('/app/', (req, res, next) => {
     res.status(200).send("200 OK")
 })
-app.get('/app/rps', (req, res, next) => {
+
+app.get('/app/rps/', (req, res, next) => {
     res.status(200).send(rps())
 })
-app.get('/app/rpsls', (req, res, next) => {
+
+app.get('/app/rpsls/', (req, res, next) => {
     res.status(200).send(rpsls())
 })
-app.post('/app/rps/play', (req, res, next) => {
-    res.status(200).send(rps(req.body.shot()))
+
+app.post('/app/rps/play/', (req, res, next) => {
+    res.status(200).send(rps(req.body.shot))
 })
-app.post('/app/rps/play', (req, res, next) => {
-    res.status(200).send(rps(req.query.shot()))
+
+app.post('/app/rps/play/', (req, res, next) => {
+    res.status(200).send(rps(req.query.shot))
 })
-app.get('/app/rps/play/:shot', (req, res, next) => {
+
+app.get('/app/rps/play/:shot/', (req, res, next) => {
     res.status(200).send(rps(req.params.shot))
 })
-app.post('/app/rpsls/play', (req, res, next) => {
+
+app.post('/app/rpsls/play/', (req, res, next) => {
     res.status(200).send(rpsls(req.body.shot))
 })
-app.post('/app/rpsls/play', (req, res, next) => {
+
+app.post('/app/rpsls/play/', (req, res, next) => {
     res.status(200).send(rpsls(req.query.shot))
 })
-app.get('/app/rpsls/play/:shot', (req, res, next) => {
+
+app.get('/app/rpsls/play/:shot/', (req, res, next) => {
     res.status(200).send(rps(req.params.shot))
 })
+
 app.get('*', (req, res, next) => {
     res.status(404).send('404 NOT FOUND')
 })
