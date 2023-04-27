8829739fc358444ce9bcfa948a89ac8b804cffbe
KrishnaPallavalli <32885657+KrishnaPallavalli@users.noreply.github.com>
2023-04-27 13:08:28 -0400

    Fixed server.js

--- server.js
+++ server.js
@@ -4,47 +4,47 @@ import express from 'express';
 import minimist from 'minimist';
 
 const app = express();
-
 const args = minimist(process.argv.slice(2));
 const PORT = args.port || 5000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
-app.get('/app/', (req, res) => {     
+
+app.get('/app/', (res) => {     
     res.status(200).send("200 OK")
 })
-app.get('/app/rps/', (req, res) => {     
+
+app.get('/app/rps/', (res) => {     
     res.status(200).send(rps(null));
 })
-app.get('/app/rpsls/', (req, res) => {   
+
+app.get('/app/rpsls/', (res) => {   
     res.status(200).send(rpsls(null));
 })
+
 app.get('/app/rps/play/', (req, res) => {   
     res.status(200).send(rps(req.query.shot));
 })
 
-app.post('/app/rps/play/', (req, res) => {   
+app.post('/app/rps/play', (req, res) => {   
     res.status(200).send(rps(req.body.shot));
 })
-
-app.get('/app/rpsls/play/', (req, res) => {   
+app.get('/app/rpsls/play', (req, res) => {   
     res.status(200).send(rpsls(req.query.shot));
 })
-
-app.post('/app/rpsls/play/', (req, res) => {   
+app.post('/app/rpsls/play', (req, res) => {   
     res.status(200).send(rpsls(req.body.shot));
 })
-
-app.get('/app/rps/play/:shot/', (req, res) => {     
+app.get('/app/rps/play/:shot', (req, res) => {     
     res.status(200).send(rps(req.params.shot));
 })
-app.get('/app/rpsls/play/:shot/', (req, res) => {   
+app.get('/app/rpsls/play/:shot', (req, res) => {   
     res.status(200).send(rpsls(req.params.shot));
 })
 
-
-app.get('*', (req, res) => {
+app.get('*', ( res) => {
     res.status(404).send('404 NOT FOUND');
   });
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 })
