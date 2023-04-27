63d3c739acad9b472da9063fa68b3696e2edf7e5
KrishnaPallavalli <32885657+KrishnaPallavalli@users.noreply.github.com>
2023-04-27 13:15:51 -0400

    Trying a solution to server.js

--- server.js
+++ server.js
@@ -2,21 +2,20 @@
 import { rps, rpsls } from './lib/rpsls.js'
 import express from 'express';
 import minimist from 'minimist';
+
 const app = express();
+
 const args = minimist(process.argv.slice(2));
 const PORT = args.port || 5000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
-
 app.get('/app/', (req, res) => {     
     res.status(200).send("200 OK")
 })
-
 app.get('/app/rps/', (req, res) => {     
     res.status(200).send(rps(null));
 })
-
-app.get('/app/rpsls/', (res) => {   
+app.get('/app/rpsls/', (req, res) => {   
     res.status(200).send(rpsls(null));
 })
 app.get('/app/rps/play/', (req, res) => {   
@@ -38,15 +37,14 @@ app.post('/app/rpsls/play/', (req, res) => {
 app.get('/app/rps/play/:shot/', (req, res) => {     
     res.status(200).send(rps(req.params.shot));
 })
-
 app.get('/app/rpsls/play/:shot/', (req, res) => {   
     res.status(200).send(rpsls(req.params.shot));
 })
 
+
 app.get('*', (req, res) => {
     res.status(404).send('404 NOT FOUND');
   });
-
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 })
