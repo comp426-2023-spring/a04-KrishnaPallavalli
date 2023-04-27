39be03dbf8a230a02ec493b6d1a92f3e88e1d9c6
KrishnaPallavalli <32885657+KrishnaPallavalli@users.noreply.github.com>
2023-04-27 14:05:11 -0400

    reverted back

--- server.js
+++ server.js
@@ -1,53 +1,33 @@
-#!/usr/bin/env node
+const args = require('minimist')(process.argv.slice(2))
+const port = args.port || 5000
 import { rps, rpsls } from './lib/rpsls.js'
-import express from 'express';
-import minimist from 'minimist';
-const app = express();
-const args = minimist(process.argv.slice(2));
-const PORT = args.port || 5000;
-app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+var express = require('express')
+var app = express()
 
-app.get('/app/', (req, res) => {
-    res.status(200).send('200 OK');
+app.get('/app', (req, res, next) => {
+    res.send("200 OK")
 })
 
-app.get('/app/rps/', (req, res) => {
-    res.status(200).send(rps());
+app.get('/app/rps', (req, res, next) => {
+    res.send(rps())
 })
 
-app.get('/app/rpsls/', (req, res) => {
-    res.status(200).send(rpsls());
+app.get('/app/rpsls', (req, res, next) => {
+    res.send(rpsls())
 })
 
-app.get('/app/rps/play/', (req, res) => {
-    res.status(200).send(rps(req.query.shot));
+app.get('/app/rps/play', (req, res, next) => {
+    res.send(rps(req.body.shot()))
 })
 
-app.get('/app/rpsls/play/', (req, res) => {
-    res.status(200).send(rpsls(req.query.shot));
+app.get('/app/rps/play/:shot', (req, res, next) => {
+    res.send(rps(req.params.shot))
 })
 
-app.post('/app/rps/play/', (req, res) => {
-    res.status(200).send(rps(req.body.shot));
+app.get('/app/rpsls/play', (req, res, next) => {
+    res.send(rpsls(req.body.shot))
 })
 
-app.post('/app/rpsls/play/', (req, res) => {
-    res.status(200).send(rpsls(req.body.shot));
-})
-
-
-app.get('/app/rps/play/:shot/', (req, res) => {
-    res.status(200).send(rps(req.params.shot));
-})
-
-app.get('/app/rpsls/play/:shot/', (req, res) => {
-    res.status(200).send(rpsls(req.params.shot));
-})
-
-app.get('*', (req, res) => {
-    res.status(404).send('404 NOT FOUND');
-  });
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`)
+app.get('/app/rpsls/play/:shot', (req, res, next) => {
+    res.send(rpsls(req.params.shot))
 })
